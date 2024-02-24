d772c3e7c959a552544069d38ef5de3a18856db3
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-24 11:37:15 +0100

    stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,14 @@ from flask import jsonify
 def status():
     """return status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """return stats"""
+    from models import storage
+    classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+               "Review": "reviews", "State": "states", "User": "users"}
+    stats = {}
+    for key, value in classes.items():
+        stats[value] = storage.count(key)
+    return jsonify(stats)
