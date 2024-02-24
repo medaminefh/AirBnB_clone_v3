e1dc617b09139ac7a7ec8b8867be26ce940bf4fc
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-24 13:03:24 +0100

    add cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 Flask rest api
 """
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
@@ -10,6 +11,7 @@ import os
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
