d7a063331c7956e113598237500b7d9abbeef0bd
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-24 11:42:36 +0100

    formatiing

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,12 +11,15 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(self):
     """call close method"""
     storage.close()
 
 # handle 404 errors
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     """return 404 error"""
@@ -27,4 +30,4 @@ if __name__ == "__main__":
     """run the app using the env variables"""
     host = os.environ.get('HBNB_API_HOST') or '0.0.0.0'
     port = os.environ.get('HBNB_API_PORT') or 5000
-    app.run(host= host, port= port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,10 +6,12 @@ Blueprint for the API
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """return status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
